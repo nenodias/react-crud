87c58799faa3a53b12bca734818a2a87eb102a24
Horácio Dias <horacio.baptista@supportweb.com>
2017-03-13 13:28:28 -0300

    Melhorando o tratamento de erros no post e delete

--- python_api/endpoints.py
+++ python_api/endpoints.py
@@ -2,7 +2,7 @@
 import logging
 from app import db
 from models import Programa
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify
 from serialize import programa_schema, programas_schema
 
 app = Blueprint('programs', __name__)
@@ -28,22 +28,16 @@ def programa_save():
             data = program_loaded.data
             program = Programa(**program_loaded.data)
             if not data.get('id'):
-                try:
-                    db.session.add(program)
-                    status = 201
-                except Exception as e:
-                    logging.error(str(e))
+                db.session.add(program)
+                status = 201
             else:
-                try:
-                    db.session.merge(program)
-                    status = 200
-                except Exception as e:
-                    logging.error(str(e))
+                db.session.merge(program)
+                status = 200
             db.session.commit()
         return programa_schema.jsonify(program), status
     except Exception as e:
         logging.error(str(e))
-        return abort(500)
+        return jsonify({"message": str(e)}), 500
 
 
 @app.route('/api/programs/<id>', methods=['GET'])
@@ -58,7 +52,11 @@ def programa_delete(id):
     """Retorna uma instância de Programa."""
     program = Programa.query.get(id)
     if program:
-        db.session.delete(program)
-        db.session.commit()
-        return "", 200
+        try:
+            db.session.delete(program)
+            db.session.commit()
+            return "", 200
+        except Exception as e:
+            logging.error(str(e))
+            return jsonify({"message": str(e)}), 500
     return "", 404
